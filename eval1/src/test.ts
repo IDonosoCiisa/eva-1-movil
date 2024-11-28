76e477f1db5471c6d27937214552b5c3ce37b820
IDonosoCiisa <ignacio.donoso.salinas@ciisa.cl>
2024-11-28 15:03:39 -0300

    [MOBILE-EVAL2]feature: test for camera capabilities

--- eval1/src/test.ts
+++ eval1/src/test.ts
@@ -6,7 +6,6 @@ import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
-
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
